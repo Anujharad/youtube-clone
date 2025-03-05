d25bc2f9a7d38538df27087790480a826dfb8169
unknown <anujharad999@gmail.com>
2025-03-05 22:29:40 +0530

    initial ommit

---
+++ src/components/videocard.tsx
@@ -0,0 +1,18 @@
+export function Videocard(){
+    return <div>
+       <img src = "video1.png"></img>
+       <div className="grid grid-cols-12">
+          <div className="col-span-1">
+            <img  className=" rounded-full w-18 h-18 pt-1 pl-1 " src="/channel1.jpg"></img>
+          </div>
+          <div className="col-span-11">
+           <div >sharktank eps01| Aman </div>
+           <div className ="text-gray-600 ">Set india</div>
+           <div className ="text-gray-600">336k views | 1 months ago</div>  
+          </div>
+         
+        </div>
+
+
+    </div>    
+}
