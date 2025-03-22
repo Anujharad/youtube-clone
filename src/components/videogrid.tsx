f692e013b160941dc2b43fce1045002ad4447fbc
unknown <anujharad999@gmail.com>
2025-03-22 18:02:34 +0530

    updated

--- src/components/videogrid.tsx
+++ src/components/videogrid.tsx
@@ -76,8 +76,22 @@ import { Videocard } from "./videocard"
         view : "336k views | 1 months ago",
         logo:"channel1.jpg",
         thumbnail:"video1.png",
+      },{     title: "sharktank eps01| Aman",
+        channel:"Set india", 
+        view : "336k views | 1 months ago",
+        logo:"channel1.jpg",
+        thumbnail:"video1.png",
+      },{     title: "sharktank eps01| Aman",
+        channel:"Set india", 
+        view : "336k views | 1 months ago",
+        logo:"channel1.jpg",
+        thumbnail:"video1.png",
+      },{     title: "sharktank eps01| Aman",
+        channel:"Set india", 
+        view : "336k views | 1 months ago",
+        logo:"channel1.jpg",
+        thumbnail:"video1.png",
       }
-    
        ]
 
        export const Videogrid = ()=>{return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
